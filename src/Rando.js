66a99d2f47352857bd886c6f91101ee6acffb6fc
brettmadrid <brettmadrid9@gmail.com>
2021-12-27 20:10:26 -0800

    created rando.js

---
+++ src/Rando.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Rando extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 0,
+    };
+    this.makeTimer();
+  }
+
+  makeTimer() {
+    setInterval(() => {
+      let random = Math.floor(Math.random() * this.props.maxNum);
+      this.setState({ num: random });
+    }, 1000);
+  }
+
+  render() {
+    return <div>{this.state.num}</div>;
+  }
+}
+
+export default Rando;
